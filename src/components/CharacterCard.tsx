49a5c571e31fd903543009a1ebc9fe5c844db49f
Lucas Misael <lucas.misael@klarna.com>
2022-12-23 16:38:35 +0100

    change character card appearance

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -11,76 +11,86 @@ import CharacterStatus from "./CharacterStatus"
 
 const styles: CustomStyles = {
   CardContainer: {
+    borderRadius: "4px",
     cursor: "pointer",
     minWidth: 275,
-    transition: "all .2s ease-in-out",
+    position: "relative",
+    transition: "all .3s ease-in-out",
 
     "&:hover": {
       boxShadow: 5,
-      transform: "scale(1.02)",
+      transform: "translateY(5px)",
+
+      ">div": {
+        opacity: 1,
+      },
+
+      "&:before": { opacity: 1 },
+    },
+
+    "&:before": {
+      backgroundColor: "rgba(0, 0, 0, 0.7)",
+      borderRadius: "4px",
+      content: '""',
+      display: "block",
+      height: "100%",
+      left: 0,
+      opacity: 0,
+      position: "absolute",
+      transition: ".5s",
+      top: 0,
+      width: "100%",
+      zIndex: 10,
     },
   },
-  CharacterCardContent: {
-    color: grey[100],
-    display: "flex",
-    padding: "0 !important",
-    backgroundColor: blueGrey["700"],
-  },
-  CardImage: {
-    height: "200px",
-    width: "200px",
-  },
   CardInfoContainer: {
+    color: "white",
     display: "flex",
     flexDirection: "column",
-    width: "100%",
-  },
-  CardSpeciesLabel: {
-    color: grey[300],
-    fontSize: ".75rem",
-    fontWeight: "500",
-    lineHeight: 1.5,
-  },
-  CardSpeciesValue: {
-    fontWeight: "700",
-    lineHeight: 1.1,
+    height: "calc(100% - 32px)",
+    justifyContent: "flex-end",
+    opacity: 0,
+    position: "absolute",
+    top: 0,
+    zIndex: 20,
+    width: "calc(100% - 32px)",
   },
   CardLocationLabel: {
     color: grey[300],
-    fontSize: ".75rem",
+    fontSize: "1rem",
     fontWeight: "500",
     lineHeight: 1.1,
     marginBottom: "4px",
-    marginTop: "auto",
   },
-  CardLocationValue: { fontSize: "1.1rem", fontWeight: "700", lineHeight: 1.1 },
-  CharacterName: { fontSize: "1.25rem", fontWeight: "600", lineHeight: 1.1 },
+  CardLocationValue: { fontSize: "1.5rem", fontWeight: "700", lineHeight: 1.1 },
+  CharacterName: { fontSize: "2rem", fontWeight: "600", lineHeight: 1.1 },
 }
 
 const CharacterIntro: FC<Pick<Character, "name" | "status" | "species">> = ({ name, status, species }) => (
-  <div aria-label='character-intro' style={{ marginBottom: "4px", position: "relative" }}>
+  <div aria-label='character-intro' style={{ marginBottom: "16px", position: "relative" }}>
     <Typography sx={styles.CharacterName}>{name}</Typography>
-    <CharacterStatus status={status} label={(status) => `${status} - ${species}`} />
+    <CharacterStatus
+      status={status}
+      getLabel={(status) => `${status} - ${species}`}
+      labelStyle={{ fontSize: "1rem" }}
+    />
   </div>
 )
 
 const CharacterCard: FC<Character & { openCharacterDialog: any }> = memo((props) => {
   const { image, location, name, openCharacterDialog, species, status } = props
-  const { CardContainer, CardImage, CardInfoContainer, CardLocationLabel, CardLocationValue, CharacterCardContent } =
-    styles
+  const { CardContainer, CardInfoContainer, CardLocationLabel, CardLocationValue } = styles
 
   return (
-    <Grid item xs={12} sm={12} md={6} xl={4} role='listitem'>
+    <Grid item xs={12} sm={12} md={6} xl={3} role='listitem'>
       <Card sx={CardContainer} onClick={() => openCharacterDialog(props)} title='Click to see more details'>
-        <CardContent sx={CharacterCardContent}>
-          <CardMedia component='img' image={image} alt={name} sx={CardImage} loading='lazy' />
-          <CardContent sx={CardInfoContainer}>
-            <CharacterIntro name={name} status={status} species={species} />
-            <Typography sx={CardLocationLabel}>Last known location:</Typography>
-            <Typography component='span' sx={CardLocationValue}>
-              {location.name}
-            </Typography>
-          </CardContent>
+        <CardMedia component='img' image={image} alt={name} loading='lazy' />
+        <CardContent sx={CardInfoContainer}>
+          <CharacterIntro name={name} status={status} species={species} />
+          <Typography sx={CardLocationLabel}>Last known location:</Typography>
+          <Typography component='span' sx={CardLocationValue}>
+            {location.name}
+          </Typography>
         </CardContent>
       </Card>
     </Grid>
--- src/components/CharacterStatus.tsx
+++ src/components/CharacterStatus.tsx
@@ -1,23 +1,35 @@
-import React, { FC } from "react"
+import { CSSProperties, FC } from "react"
 import Typography from "@mui/material/Typography"
 import { getStatusColor } from "../utils"
 
-const CharacterStatus: FC<{ status: string; label?: (status: string) => string }> = ({ status, label }) => (
-  <>
-    <div
-      style={{
-        backgroundColor: getStatusColor(status),
-        borderRadius: "50%",
-        display: "inline-block",
-        height: "8px",
-        marginRight: "5px",
-        width: "8px",
-      }}
-    />
-    <Typography sx={{ fontSize: ".9rem", fontWeight: "500", lineHeight: 1.5, display: "inline" }}>
-      {label ? label(status) : status}
-    </Typography>
-  </>
-)
+const getDefaultStyles: (status: string) => { [key: string]: CSSProperties } = (status) => ({
+  dot: {
+    backgroundColor: getStatusColor(status),
+    borderRadius: "50%",
+    display: "inline-block",
+    height: "8px",
+    marginBottom: "2px",
+    marginRight: "5px",
+    verticalAlign: "middle",
+    width: "8px",
+  },
+  label: { fontSize: ".9rem", fontWeight: "500", lineHeight: 1.5, display: "inline", textTransform: "capitalize" },
+})
+
+const CharacterStatus: FC<{
+  status: string
+  getLabel?: (status: string) => string
+  labelStyle?: CSSProperties
+  dotStyle?: CSSProperties
+}> = ({ status, getLabel, labelStyle = {}, dotStyle = {} }) => {
+  const { dot, label } = getDefaultStyles(status)
+
+  return (
+    <>
+      <div style={{ ...dot, ...dotStyle }} />
+      <Typography sx={{ ...label, ...labelStyle }}>{getLabel ? getLabel(status) : status}</Typography>
+    </>
+  )
+}
 
 export default CharacterStatus
