7738b9defa6adb4842457903ef874f8084f37565
Lucas Misael <lucas.misael@klarna.com>
2022-12-24 11:04:41 +0100

    fix header animations

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,84 +1,88 @@
-import React, { FC, CSSProperties, useEffect, useState, memo } from "react"
+import React, { FC, useEffect, useState, memo } from "react"
 import Box from "@mui/material/Box"
 import Container from "@mui/material/Container"
-import { SxProps } from "@mui/material"
 import Search from "@mui/icons-material/Search"
-import { useDebounce } from "../utils"
+import { CustomStyles, useDebounce } from "../utils"
 import { Theme, useTheme } from "@mui/material/styles"
 
-const getHeaderStyle: (isVisible: string, theme: Theme) => SxProps = (isVisible, theme) => ({
-  backgroundColor: "rgba(0, 0, 0, .9)",
-  height: "185px",
-  position: "fixed",
-  top: isVisible === "full" ? 0 : isVisible === "no" ? "-300px" : "-200px",
-  transitionProperty: "all",
-  transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
-  transitionDuration: "500ms",
-  width: "100%",
-  zIndex: 30,
+const getStyles: (isVisible: string, theme: Theme) => CustomStyles = (isVisible, theme) => ({
+  Header: {
+    backgroundColor: "rgba(0, 0, 0, .9)",
+    height: "185px",
+    position: "fixed",
+    top: isVisible === "full" ? 0 : isVisible === "no" ? "-250px" : "-105px",
+    transitionProperty: "all",
+    transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+    transitionDuration: "500ms",
+    width: "100%",
+    zIndex: 30,
 
-  [theme.breakpoints.up("sm")]: {
-    height: "250px",
-  },
-})
-
-const getContainerStyle: (theme: Theme) => SxProps = (theme) => ({
-  alignItems: "center",
-  display: "flex",
-  flexDirection: "column",
-  height: "100%",
-  justifyContent: "space-between",
-
-  img: {
-    marginTop: "8px",
-    width: "300px",
     [theme.breakpoints.up("sm")]: {
-      width: "500px",
+      top: isVisible === "full" ? 0 : isVisible === "no" ? "-250px" : "-170px",
+      height: "250px",
     },
   },
-})
 
-const searchBoxStyle: SxProps = {
-  alignItems: "center",
-  backgroundColor: "rgba(255, 255, 255, 0.5)",
-  borderRadius: "10px",
-  color: "white",
-  display: "flex",
-  marginBottom: "24px",
-  position: "relative",
-  width: "100%",
+  HeaderContent: {
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    justifyContent: "space-between",
 
-  svg: {
-    fontSize: "1.75rem",
-    marginRight: "4px",
-    position: "absolute",
-    right: 4,
+    img: {
+      marginTop: "8px",
+      width: "300px",
+      [theme.breakpoints.up("sm")]: {
+        width: "500px",
+      },
+    },
   },
 
-  "& input": {
-    backgroundColor: "transparent",
-    border: 0,
+  SearchBox: {
+    alignItems: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
     borderRadius: "10px",
-    fontFamily: "Montserrat, sans-serif",
-    fontSize: "1.25rem",
-    height: "100%",
-    padding: "16px 12px",
+    color: "white",
+    display: "flex",
+    margin: "12px 0",
+    position: "relative",
     width: "100%",
 
-    "::placeholder": {
-      color: "white",
+    svg: {
+      fontSize: "1.75rem",
+      marginRight: "4px",
+      position: "absolute",
+      right: 4,
     },
 
-    ":focus": {
-      outlineWidth: "thin",
+    "& input": {
+      backgroundColor: "transparent",
+      border: 0,
+      borderRadius: "10px",
+      fontFamily: "Montserrat, sans-serif",
+      fontSize: "1.25rem",
+      height: "100%",
+      padding: "16px 12px",
+      width: "100%",
+
+      "::placeholder": {
+        color: "white",
+      },
+
+      ":focus": {
+        outlineWidth: "thin",
+      },
     },
   },
-}
+})
 
 const Header: FC<{ onSearch: (value: string) => void }> = memo(({ onSearch }) => {
   const theme = useTheme()
   const [isHeaderVisible, setIsHeaderVisible] = useState("full")
   const [value, setValue] = useState("")
+
+  const { Header, HeaderContent, SearchBox } = getStyles(isHeaderVisible, theme)
 
   useEffect(() => {
     let lastScrollY = window.pageYOffset
@@ -106,10 +110,10 @@ const Header: FC<{ onSearch: (value: string) => void }> = memo(({ onSearch }) =>
   useDebounce(() => onSearch(value), [value], 500)
 
   return (
-    <Box component='header' sx={getHeaderStyle(isHeaderVisible, theme)}>
-      <Container maxWidth='xl' sx={getContainerStyle(theme)}>
+    <Box component='header' sx={Header}>
+      <Container maxWidth='xl' sx={HeaderContent}>
         <img src={process.env.PUBLIC_URL + "/img/logo.webp"} alt='Rick and Morty logo' />
-        <Box sx={searchBoxStyle}>
+        <Box sx={SearchBox}>
           <input placeholder='Find a character' aria-label='ok' onChange={(e) => setValue(e.target.value)} />
           <Search />
         </Box>
